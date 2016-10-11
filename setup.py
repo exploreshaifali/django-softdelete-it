f2f51d7b6dccc8bbcd26df57ed74f962ca90af9e
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2016-10-11 23:54:51 +0530

    configs for pip modified to use rst README

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-sofedelete-it',
+    name='django-softdelete-it',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
@@ -22,9 +22,10 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: soft-delete',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GPL Compatible :: MIT License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.5',
     ],
